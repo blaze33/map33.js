fd45c8a4f180eab0c0cc2273dc2664abdeb118da
Maxime Rouyrre <rouyrre+git@gmail.com>
2020-07-07 01:57:18 +0200

    code cleanup

--- src/land/index.js
+++ src/land/index.js
@@ -3,40 +3,21 @@ import Stats from "stats.js";
 import {
   Scene,
   PerspectiveCamera,
-  Vector2,
   Vector3,
-  Matrix4,
   WebGLRenderer,
   PCFSoftShadowMap,
   ACESFilmicToneMapping,
   Color,
   FogExp2,
-  Mesh,
-  SphereBufferGeometry,
-  MeshBasicMaterial,
-  WebGLRenderTarget,
-  DepthTexture,
-  MeshDepthMaterial,
 
-  // Water imports
-  PlaneBufferGeometry,
-  TextureLoader,
-  RepeatWrapping,
-  LOD,
   LinearEncoding,
-  MeshNormalMaterial,
-  MeshPhongMaterial,
-  MeshStandardMaterial,
   AmbientLight,
   DirectionalLight,
   AxesHelper
 } from "three";
-// import * as THREE from 'three'
 import {
-  OrbitControls,
   MapControls,
 } from "three/examples/jsm/controls/OrbitControls.js";
-import ThreeGeo from "three-geo/src";
 
 import InfiniteGridHelper from "./three.modules/InfiniteGridHelper";
 
@@ -110,90 +91,12 @@ scene.add(dirLight)
 const grid = new InfiniteGridHelper(100, 500);
 scene.add(grid);
 
-const material = new MeshStandardMaterial({
-  metalness: 0.5,
-  roughness: 0.5
-});
-const buildTile = (heightmap, size, segments) => {
-  const geometry = new PlaneBufferGeometry(size, size, segments + 2, segments + 2)
-  const nPosition = Math.sqrt(geometry.attributes.position.count)
-  const nHeightmap = Math.sqrt(heightmap.length)
-  const ratio = nHeightmap / (nPosition)
-  let x, y
-  for (let i = nPosition; i < geometry.attributes.position.count - nPosition; i++) {
-    if (
-      i % (nPosition) === 0 ||
-      i % (nPosition) === nPosition - 1
-      ) continue
-      x = Math.floor(i / (nPosition))
-      y = i % (nPosition)
-      geometry.attributes.position.setZ(
-    i,
-    heightmap[Math.round(Math.round(x * ratio) * nHeightmap + y * ratio)] * 0.075
-    )
-  }
-  geometry.computeVertexNormals()
-  const mesh = new Mesh(geometry, material);
-  mesh.receiveShadow = true
-  mesh.receiveShadow = true
-  return mesh
-}
-
-// const tileURL =
-//   "https://s3.amazonaws.com/elevation-tiles-prod/terrarium/10/531/365.png";
-
-// let dem2mesh;
-// const initdem2mesh = import("dem2mesh").then((pkg) => {
-//   dem2mesh = pkg;
-//   dem2mesh.init();
-// });
-// // dem2mesh.init()
-// const fetchpng = fetch(tileURL)
-//   .then((res) => res.arrayBuffer())
-//   .then((arrayBuffer) => new Uint8Array(arrayBuffer))
-// Promise.all([initdem2mesh, fetchpng]).then(([_, png]) => {
-//   const heightmap = dem2mesh.png2elevation(png)
-//   const tile = buildTile(heightmap, 1000, 254)
-//   scene.add(tile)
-//   window.tile = tile
-// })
-window.ThreeGeo = ThreeGeo
-const tgeo = new ThreeGeo({
-  tokenMapbox: 'pk.eyJ1IjoibWF4bXJlIiwiYSI6ImNrYzd5MTM1cTEzbmUyc3FwazZjYWxrb3UifQ.Gp9n0wzvtr5Dm1tB4_KDRg',                // <---- set your Mapbox API token here
-  unitsSide: 1.0,
-});
-
-// params: [lat, lng], terrain's radius (km), satellite zoom resolution, callbacks
-// Beware the value of radius; for zoom 12, radius > 5.0 (km) could trigger huge number of tile API calls!!
-// const radius = 1e-10
-const radius = 5.
-const onRgbDemCb = (meshes) => {
-  meshes.forEach((mesh) => {
-    const scale = radius * Math.pow(2, 0.5) * 100;
-    mesh.scale.set(scale, scale, scale);
-    scene.add(mesh);
-  });
-  window.meshes = meshes;
-};
 // let position = [46.5763, 7.9904]
 // let position = [45.8671, 7.3087]
 let position = [45.916216, 6.860973];
-// tgeo.getTerrain(position, radius, 12, {
-//   onRgbDem: onRgbDemCb,
-//   onSatelliteMat: mesh => {                 // your implementation when terrain's satellite texture is obtained
-//     // const map = mesh.material.map
-//     // const material = new MeshStandardMaterial({
-//     //   metalness: 0.15,
-//     //   roughness: 0.4,
-//     //   map,
-//     //   // normalMap: map
-//     // });
-//     // mesh.material = material
-//   },
-// });
-window.tgeo = tgeo
 
-const map = new Map(scene, camera, position, 4, 11)
+const map = new Map(scene, camera, position, 2, 11)
+window.map = map
 const mapPicker = new MapPicker(camera, map, renderer.domElement)
 console.log(map)
 
