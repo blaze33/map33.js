8ec6d9e544070560827d5cb13a13ddc04c68bc3b
Maxime Rouyrre <rouyrre+git@gmail.com>
2020-07-07 14:00:34 +0200

    Updates

---
+++ precache-manifest.f5bb67b058822636bbcfbeb337afba1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9637ae246c832cfe277dd081e1a048ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea802f32b1a5b8c82f63",
+    "url": "/static/css/2.af38336f.chunk.css"
+  },
+  {
+    "revision": "d7ae845d0bdfd328a342",
+    "url": "/static/css/main.67f5513c.chunk.css"
+  },
+  {
+    "revision": "ea802f32b1a5b8c82f63",
+    "url": "/static/js/2.bd66d220.chunk.js"
+  },
+  {
+    "revision": "ab2274a52de22bf882d8ba14f813e7a5",
+    "url": "/static/js/2.bd66d220.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7ae845d0bdfd328a342",
+    "url": "/static/js/main.4f2433c6.chunk.js"
+  },
+  {
+    "revision": "f4525e1a4be3a9c349c1",
+    "url": "/static/js/runtime-main.a8cbdcbb.js"
+  }
+]);
